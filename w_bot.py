d9d5598fabb6a125964749e4992b5e7ca33e302a
3koozy <3koozy@gmail.com>
2023-03-25 15:56:27 -0400

    Update w_bot.py

--- w_bot.py
+++ w_bot.py
@@ -8,20 +8,38 @@ openai.api_key = os.environ['openai_api_key']
 
 app = Flask(__name__)
 
+#bot role:
+bot_role = "You are a helpful assistant."
+
 @app.route('/bot', methods=['POST'])
 def bot():
     # Get the user's message from the incoming request
     user_msg = request.form.get('Body')
+    user_name = request.form.get('ProfileName') or 'User'
 
-    # Call GPT-3.5 API
-    response = openai.ChatCompletion.create(
-      model='gpt-3.5-turbo',
-      messages = [{"role": "system", "content": "You are a helpful assistant."},
-                    {"role": "user", "content": user_msg}],
-      max_tokens= 250,
-      temperature= 0.7,
-      n= 1
-    )
+    # Check if the message is from a group chat
+    is_group = request.form.get('NumMedia') == '0' and 'ProfileName' in request.form
+
+    if is_group:
+        # Call GPT-3.5 API
+        response = openai.ChatCompletion.create(
+          model='gpt-3.5-turbo',
+          messages = [{"role": "system", "content": bot_role},
+                        {"role": "user", "content": f"{user_name} in a group chat asks: {user_msg}"}],
+          max_tokens= 250,
+          temperature= 0.7,
+          n= 1
+        )
+    else:
+        # Call GPT-3.5 API
+        response = openai.ChatCompletion.create(
+          model='gpt-3.5-turbo',
+          messages = [{"role": "system", "content": bot_role},
+                        {"role": "user", "content": user_msg}],
+          max_tokens= 250,
+          temperature= 0.7,
+          n= 1
+        )
 
     gpt_response = response['choices'][0]['message']['content'].strip()
 
